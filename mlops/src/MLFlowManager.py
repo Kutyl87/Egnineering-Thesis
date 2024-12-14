121c78fc1bc18717b86d4db442b3a4aa06e2451d
pkutyl <01168894@pw.edu.pl>
2024-12-14 22:34:49 +0100

    Add minimal code for training

--- mlops/src/MLFlowManager.py
+++ mlops/src/MLFlowManager.py
@@ -1,7 +1,10 @@
 import logging
+import stat
+
 import mlflow
 import mlflow.pyfunc
-from ModelWrapper import ModelWrapper
+import os
+from mlops.src.ModelWrapper import ModelWrapper
 
 
 class MLFlowManager:
@@ -15,24 +18,28 @@ class MLFlowManager:
             self.logger.info(f"Tracking URI set to: {tracking_uri}")
         else:
             self.logger.warning("No tracking URI provided. Defaulting to local.")
-
+        self.experiment_name = experiment_name
         self.logger.info(f"Experiment set to: {experiment_name}")
         mlflow.set_experiment(experiment_name=experiment_name)
 
-    def log_model(self, model: ModelWrapper, model_name: str) -> None:
-        with mlflow.start_run() as run:
-            mlflow.pyfunc.log_model(artifact_path=model_name, python_model=model)
-            self.logger.info(
-                f"Model '{model_name}' logged under run ID: {run.info.run_id}"
-            )
+    def log_model(self, model: ModelWrapper, model_name: str, run: any) -> None:
+        # with mlflow.start_run() as run:
+        mlflow.pyfunc.log_model(artifact_path=model_name, python_model=model)
+        self.logger.info(
+            f"Model '{model_name}' logged under run ID: {run.info.run_id}"
+        )
 
     def log_artifacts(self, artifact_path: str) -> None:
         mlflow.log_artifacts(artifact_path)
         self.logger.info(f"Artifacts logged from path: {artifact_path}")
 
-    def log_metrics(self, metrics: dict) -> None:
-        mlflow.log_metrics(metrics)
-        self.logger.info(f"Metrics logged: {metrics}")
+    def log_metrics(self, metrics: dict, step=None) -> None:
+        if step is not None:
+            mlflow.log_metrics(metrics, step=step)
+            self.logger.info(f"Metrics logged: {metrics} at step: {step}")
+        else:
+            mlflow.log_metrics(metrics)
+            self.logger.info(f"Metrics logged: {metrics}")
 
     def log_params(self, params: dict) -> None:
         mlflow.log_params(params)
@@ -48,3 +55,7 @@ class MLFlowManager:
             self.logger.error(
                 f"Failed to load model '{model_name}' from '{model_uri}': {e}"
             )
+
+    def log_fig(self, fig, fig_name: str) -> None:
+        mlflow.log_figure(fig, fig_name)
+
